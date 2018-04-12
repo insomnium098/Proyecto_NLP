205ef516e2397c054d109f9ee71b6f297f08960f
Antonio Martinez <insomnium098@gmail.com>
2018-04-12 10:09:10 -0500

    Carpetas necesarias

--- nlp-preprocessing-pipeline.sh
+++ nlp-preprocessing-pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo 'Preprocessing files...'
-ORIGINAL_CORPUS_PATH=/export/space1/users/compu2/bionlp/conditional-random-fields/data-sets/original
-CORPUS_PATH=/export/space1/users/compu2/bionlp/conditional-random-fields/data-sets
-TERM_PATH=/export/space1/users/compu2/bionlp/nlp-preprocessing-pipeline/dictionaries
-POST_PATH=/export/space1/users/compu2/bionlp/stanford-postagger-2018-02-27
-LEMMA_PATH=/export/space1/users/compu2/bionlp/biolemmatizer
+ORIGINAL_CORPUS_PATH=/Users/daniel/Desktop/ProyectoNLP/ARCHIVOS_NECESARIOS/ORIGINAL_CORPUS_PATH/original
+CORPUS_PATH=/Users/daniel/Desktop/ProyectoNLP/conditional-random-fields/data-sets
+TERM_PATH=/Users/daniel/Desktop/ProyectoNLP/dictionaries
+POST_PATH=/Users/daniel/Desktop/ProyectoNLP/ARCHIVOS_NECESARIOS/POST_PATH/stanford-postagger-2018-02-27
+LEMMA_PATH=/Users/daniel/Desktop/ProyectoNLP/ARCHIVOS_NECESARIOS/LEMMA/biolemmatizer
 
 PRE=TRUE
 echo "   Preprocessing: $PRE"
@@ -21,33 +21,33 @@ if [ "$PRE" = "TRUE" ]; then
 echo "Preprocessing..."
 INPUT_PATH=$ORIGINAL_CORPUS_PATH
 OUTPUT_PATH=$CORPUS_PATH/preprocessed
-python3.4 preprocessingTermDetection.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --termDetection --termPath $TERM_PATH --termFiles termFilesLength.json > outputPreprocessing.txt
+python3.5 preprocessingTermDetection.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --termDetection --termPath $TERM_PATH --termFiles termFilesLength.json > outputPreprocessing.txt
 fi
 
 if [ "$POS" = "TRUE" ]; then
 echo "POS Tagging..."
 INPUT_PATH=$CORPUS_PATH/preprocessed
 OUTPUT_PATH=$CORPUS_PATH/pos
-python3.4 posTaggingStanford.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --taggerPath $POST_PATH --biolemmatizer > outputPOST.txt
+python3.5 posTaggingStanford.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --taggerPath $POST_PATH --biolemmatizer > outputPOST.txt
 fi
 
 if [ "$LEMMA" = "TRUE" ]; then
 echo "Lemmatization..."
 INPUT_PATH=$CORPUS_PATH/pos
 OUTPUT_PATH=$CORPUS_PATH/lemma
-python3.4 biolemmatizing.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --biolemmatizerPath $LEMMA_PATH  > outputLemma.txt
+python3.5 biolemmatizing.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --biolemmatizerPath $LEMMA_PATH  > outputLemma.txt
 fi
 
 if [ "$TERM" = "TRUE" ]; then
 echo "Terminological tagging..."
 INPUT_PATH=$CORPUS_PATH/lemma
 OUTPUT_PATH=$CORPUS_PATH/term
-python3.4 biologicalTermTagging-CRF.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --termPath $TERM_PATH --termFiles termFilesTag.json > outputTerm.txt
+python3.5 biologicalTermTagging-CRF.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --termPath $TERM_PATH --termFiles termFilesTag.json > outputTerm.txt
 fi
 
 if [ "$TRANS" = "TRUE" ]; then
 echo "Transformation..."
 INPUT_PATH=$CORPUS_PATH/term
 OUTPUT_PATH=$CORPUS_PATH/transformed
-python3.4 transforming-CRF.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --minWordsInLine 5 > outputTransformation.txt
+python3.5 transforming-CRF.py --inputPath $INPUT_PATH --outputPath $OUTPUT_PATH --minWordsInLine 5 > outputTransformation.txt
 fi
